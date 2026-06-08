068ac9150c2a8e06e1da9ef9894ec741d4b8c048
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-06-08 23:07:08 +0530

    Built content-based movie recommendation engine

--- python/practice_project/recommendation_system/recommendation_system.py
+++ python/practice_project/recommendation_system/recommendation_system.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 print("🎬 Movie Recommendation System Started\n")
 
-# Movie Dataset
+# Dataset
 
 data = {
     "Movie": [
@@ -35,4 +35,37 @@ df = pd.DataFrame(data)
 
 print("Dataset Loaded Successfully ✅\n")
 
-print(df)
+
+# Function to Recommend Movies
+
+def recommend_movies(movie_name):
+
+    # Find genre of selected movie
+    selected_genre = df[df["Movie"] == movie_name]["Genre"].values[0]
+
+    print(f"\n🎯 Selected Movie: {movie_name}")
+    print(f"🎭 Genre: {selected_genre}\n")
+
+    # Recommend similar movies
+    recommendations = df[
+        (df["Genre"] == selected_genre) &
+        (df["Movie"] != movie_name)
+    ]
+
+    print("🎬 Recommended Movies:\n")
+
+    for movie in recommendations["Movie"]:
+        print("👉", movie)
+
+
+# User Input
+
+print("Enter a movie name from dataset:")
+user_movie = input()
+
+# Check and Recommend
+
+if user_movie in df["Movie"].values:
+    recommend_movies(user_movie)
+else:
+    print("❌ Movie not found in dataset")
